09c5d16509ad8fa3c8574718771f55ca81045f8e
Acsimon112 <161263170+Acsimon112@users.noreply.github.com>
2025-11-03 13:44:28 -0800

    Event framework (#34)
    * Event Framework class added
    
    * added support for optional datatypes, UID generation & Timestamp creation
    
    * added default contructor method
    
    * fixed typo on import in tasks.tsx

---
+++ app/utils/eventFramework.tsx
@@ -0,0 +1,65 @@
+/*class for events to work across the entire app hopefully. Ideally stores all necessary information from .ics format
+and allows it to be used elsewhere.
+*/
+
+export default class Event {
+    // Variables directly follow .ics format
+
+    private uid!: string; //unique identifier for event
+    private DTStamp!: Date; //when the event was last modified
+    private DTstart!: string;//when the event begins
+    private DTend!: string;//when the event ends
+    private summary!: string; //title of the even, e.x. "Trevor's Bday"
+    private description?: string; //event details, e.x. "Bring cake"
+    private location?: string;//where the event occurs.
+    private status?: "CONFIRMED" | "CANCELLED" | "TENTATIVE"; //status can only be one of three things.
+    private rRule?: string; //used to set event frequency e.x. "FREQ=WEEKLY;BYDAY=MO".
+    private attendees?: string[]; //list of attendees.
+    private creator!: string; //name of event creator.
+
+    defconstructor(){
+        this.uid = `${Date.now()}`;
+        this.DTStamp = new Date(); //set current timestamp on creation
+        this.DTstart = "TZID=America/New_York:20130802T103400";
+        this.DTend = "TZID=America/New_York:20130802T110400";
+        this.summary = "Event Name";
+        this.creator = "Sam";
+        this.description = "This is a test description";
+        this.location = "Somewhere";
+        this.status = "TENTATIVE";
+        this.rRule = "";
+        this.attendees = ["Dan", "Hellen", "Phillipe", "Aubrey", "Leigh", "Lyle"];
+    }
+
+    constructor(
+        id: string,  
+        Start: string, 
+        End: string, 
+        Sum: string,    
+        founder: string,
+        desc?: string,
+        loc?: string,
+        stat?:"CONFIRMED" | "CANCELLED" | "TENTATIVE",
+        recur?: string,
+        participantList?: string[],
+        )
+    {
+        //required values
+        //Creates a random UID for the event, current date + random number
+        this.uid = `${Date.now()}`;
+        this.DTStamp = new Date(); //set current timestamp on creation
+        this.DTstart = Start;
+        this.DTend = End;
+        this.summary = Sum;
+        this.creator = founder;
+
+        // optional values
+
+        this.description = desc;
+        this.location = loc;
+        this.status = stat;
+        this.rRule = recur;
+        this.attendees = participantList;
+    }
+
+}
